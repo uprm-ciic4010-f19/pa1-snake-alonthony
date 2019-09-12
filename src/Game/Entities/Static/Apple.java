66cb4bded6a572e08ed30cd97a3ad50ce5a00080
Anthony-Mendez <anthony.mendez3@upr.edu>
2019-09-12 00:52:05 -0400

    re-established var vals

--- src/Game/Entities/Static/Apple.java
+++ src/Game/Entities/Static/Apple.java
@@ -7,11 +7,13 @@ import Main.Handler;
  */
 public class Apple {
 
+    private Handler handler;
+
     public int xCoord;
     public int yCoord;
 
-    
     public Apple(Handler handler,int x, int y){
+        this.handler=handler;
         this.xCoord=x;
         this.yCoord=y;
     }
